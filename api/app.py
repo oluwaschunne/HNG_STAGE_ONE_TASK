8fcda8b2a189f0e0b9bfa80c4fb64f4dcf9d2d13
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-01 13:12:15 +0100

    updated

--- api/app.py
+++ api/app.py
@@ -7,13 +7,22 @@ app = Flask(__name__)
 def hello():
     visitor_name = request.args.get('visitor_name', 'Guest')
     client_ip = request.remote_addr
-    api_key = '9ebe49d50b414663bfd91107240107'
+    api_key = "9ebe49d50b414663bfd91107240107"
+
+    if client_ip == "127.0.0.1":
+        client_ip = "8.8.8.8"
 
     # Get weather and location information
     weather_response = requests.get(f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={client_ip}')
     weather_data = weather_response.json()
-    city = weather_data['location']['name']
-    temperature = weather_data['current']['temp_c']
+
+    if 'location' in weather_data and 'current' in weather_data:
+        city = weather_data['location']['name']
+        temperature = weather_data['current']['temp_c']
+    else:
+        print(f"Error: Unable to get location and temperature for IP {client_ip}. API response:{weather_data}")
+        city = "Unknown"
+        temperature = "N/A"
 
     response_data = {
         'client_ip': client_ip,
