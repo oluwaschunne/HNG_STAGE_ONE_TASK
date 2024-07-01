9de11975ff2766f9efbb55d686c42d2e33b462e2
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-07-01 10:20:20 +0100

    api created

---
+++ api/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    visitor_name = request.args.get('visitor_name', 'Guest')
+    client_ip = request.remote_addr
+
+    # Get weather and location information
+    weather_response = requests.get(f'http://api.weatherapi.com/v1/current.json?key=9ebe49d50b414663bfd91107240107&q={client_ip}')
+    weather_data = weather_response.json()
+    city = weather_data['location']['name']
+    temperature = weather_data['current']['temp_c']
+
+    response_data = {
+        'client_ip': client_ip,
+        'location': city,
+        'greeting': f'Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}'
+    }
+
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
